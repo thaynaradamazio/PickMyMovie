d50147f7ad61f2981019fb5d6c33f8b4e8f0207a
thaynaradamazio <thaydamazio2015@gmail.com>
2025-04-09 13:17:35 -0300

    Initial Commit

---
+++ src/PickMyMovie.Domain/Entities/Rule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickMyMovie.Domain.Entities
+{
+    internal class Rule
+    {
+    }
+}
